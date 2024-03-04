2347e8de5482a7826b63afc0e9f22a8e7a86b21c
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-03-04 20:40:27 +0300

    Alx-frontend

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const allEmployees = reportWithIterator.allEmployees;
-  let employeeNames = [];
-
-  for (let department in allEmployees) {
-    employeeNames.push(...allEmployees[department]);
-  }
-
-  return employeeNames.join(' | ');
+  return reportWithIterator.join(' | ');
 }
