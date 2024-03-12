fa8fbbeab152f3eb8039208e988800f91463edad
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-03-13 00:00:27 +0300

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,13 +1,3 @@
-:
-
-
- h-javascript/0x03-ES6_data_manipulation$ git push
-Enumerating objects: 6, done.
-Counting objects: 100% (6/6), done.
-
-
-  return [];
--javascript/0x03-ES6_data_manipulation$ git push
-Enumerating objects: 6, done.
-Counting objects: 100% (6/6), done.
-Delta compression using up to 4 }
+export default function getStudentsByLocation(lst, city) {
+  return lst.filter((obj) => obj.location === city);
+}  
